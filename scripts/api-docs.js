d4209c647d20d252cc748042538426f0a0cd006c
Scott Yearsley <s.yearsley@zengenti.com>
2017-07-13 11:26:04 +0100

    Script fixes

--- scripts/api-docs.js
+++ scripts/api-docs.js
@@ -1,17 +1,18 @@
 (function(){
+    var urlRoot = "/contensis/api/delivery/dotnet/v/"
     var cookieName = 'api_docs_version';
     var versions = [];
     var select;
     var config;
 
     var getConfig = function() {
-        return fetch('/config.json').then(function(response) {
+        return fetch(urlRoot + 'config.json').then(function(response) {
             return response.json();
         });
     };
 
     var renderOptions = function() {
-        var storedVersion = getCookie(cookieName);
+        var selectedVersion = getCookie(cookieName);
 
         for(var i = select.options.length - 1 ; i >= 0 ; i--)
         {
@@ -19,14 +20,14 @@
         }
 
         // Add defaults
-        addSelectOption('Latest (' + config.version + ')', config.version, storedVersion === config.version);
-        addSelectOption('beta', 'Beta', storedVersion === 'Beta');
+        addSelectOption('Latest (' + config.version + ')', config.version, selectedVersion === config.version);
+        addSelectOption('beta', 'Beta', selectedVersion === 'Beta');
         
         var ordered = versions.sort(function(a, b){return b-a});
         for (var index = 0; index < ordered.length; index++) {
             var element = ordered[index];
             
-            addSelectOption(element, element, storedVersion === element);
+            addSelectOption(element, element, selectedVersion === element);
         }
     };
 
@@ -63,22 +64,26 @@
         return fetch(getPageUrl(version), options).then(function(response){
             return response.ok;
         }).catch(function(){
-            return response.ok;
+            return false;
         })
     };
 
     var getPageUrl = function(version) {
-        return config.urlRoot + version + window.location.pathname;
+        return urlRoot + version + window.location.pathname;
     };
 
-    var navigateToVersion = function(e) {
+    var navigateToVersion = function(version) {
         if (!config) {
             return;
         }
 
-        setCookie(cookieName, e.target.value);
-        console.log(config.urlRoot);
-        window.location.href = getPageUrl(e.target.value);
+        setCookie(cookieName, version);
+        console.log(urlRoot);
+        window.location.href = getPageUrl(version);
+    };
+
+    var onSelectChange = function(e) {
+        navigateToVersion(e.target.value);
     };
 
     var createSelect = function() {
@@ -88,7 +93,7 @@
         var div = document.createElement("div");
         select = document.createElement("select");
         select.setAttribute("id", "soflow");
-        select.addEventListener('change', navigateToVersion);
+        select.addEventListener('change', onSelectChange);
 
         div.appendChild(select);
         div.setAttribute("class", "version-select pull-right");
@@ -97,20 +102,6 @@
 
         renderOptions();
         discoverVersions(config);
-    };
-
-    var init = function() {
-        select = null;
-        versions = [];
-
-        if (config == null) {
-            getConfig().then(function(c){
-                config = c;
-                createSelect();
-            });
-        } else {
-            createSelect();
-        }
     };
 
     var setCookie = function setCookie(cname, cvalue) {
@@ -131,6 +122,37 @@
             }
         }
         return "";
+    };
+
+    var ensureVersion = function() {
+        var storedVersion = getCookie(cookieName);
+        if (storedVersion === "") {
+            console.log("none");
+            return;
+        }
+
+        if (window.location.pathname.indexOf(storedVersion) < 0) {
+            console.log("navigate");
+            navigateToVersion(storedVersion);
+        }
+
+        console.log("duh");
+    };
+
+    var init = function() {
+        select = null;
+        versions = [];
+
+        if (config == null) {
+            getConfig().then(function(c){
+                config = c;
+                ensureVersion();
+                createSelect();
+            });
+        } else {
+            ensureVersion();
+            createSelect();
+        }
     };
 
     gitbook.events.bind("page.change", init);
