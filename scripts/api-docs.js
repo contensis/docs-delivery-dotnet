9a918903199dd0bda65f3dd1c6af72301e498560
Scott Yearsley <s.yearsley@zengenti.com>
2017-09-13 10:46:36 +0100

    Updated version no

--- scripts/api-docs.js
+++ scripts/api-docs.js
@@ -1,6 +1,6 @@
 (function(){
     var urlRoot = "/contensis/api/delivery/dotnet/"
-    var version = 'beta'
+    var version = '10.0'
 
     var renderVersionSelectorScript = function() {
         var element = document.createElement("script");
